6f72f579e181cee4f52ec032415e787851d0b88c
kingwax <kingwax@163.com>
2015-03-25 18:43:47 +0800

    Signed-off-by: kingwax <kingwax@163.com>

--- src/com/example/a1/MainActivity.java
+++ src/com/example/a1/MainActivity.java
@@ -1,48 +1,37 @@
 package com.example.a1;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
-import org.w3c.dom.Text;
-
-import android.R.integer;
-import android.R.string;
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.AlertDialog;
-import android.app.Fragment;
 import android.app.AlertDialog.Builder;
+import android.app.Fragment;
 import android.content.DialogInterface;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.KeyEvent;
+import android.os.Environment;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.os.Build;
 
 public class MainActivity extends Activity 
 {
-
-	MediaPlayer mprainlake;  
+	//MediaPlayer mprainlake;  
 	MediaPlayer mprain; 
 	MediaPlayer mplake; 
-	
+	Boolean israinplay = false;
+    Boolean islakeplay = false;
+    float lakevol=(float)0.1;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,17 +42,16 @@ public class MainActivity extends Activity
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-        
-        
-        	mprainlake=MediaPlayer.create(this,R.raw.rainlake);
-        	mprainlake.setOnCompletionListener(mplistener);
-        	
+         	
         	mprain=MediaPlayer.create(this,R.raw.rainsound);
         	mprain.setOnCompletionListener(mplistener);
         	
         	mplake=MediaPlayer.create(this,R.raw.lake);
         	mplake.setOnCompletionListener(mplistener);
-
+        	createSDCardDir();
+        	
+        	this.registerForContextMenu(findViewById(R.id.button3));
+        	this.registerForContextMenu(findViewById(R.id.button4));
     }
 
     MediaPlayer.OnCompletionListener mplistener = new MediaPlayer.OnCompletionListener() {
@@ -75,14 +63,15 @@ public class MainActivity extends Activity
 		}
 	};
     
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+	
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        menu.add("下载其他音乐");
-        //menu.add(1, 1, 1, "item 1");
-        //menu.add(1, 2, 2, "item 2");
+        //menu.add("下载其他音乐");
+        menu.add(1, 1, 1, "鋼琴");
+        menu.add(1, 2, 2, "古琴");
         //menu.add(2, 3, 3, "item 3");
         //menu.add(2, 4, 4, "item 4");
         return true;
@@ -103,12 +92,15 @@ public class MainActivity extends Activity
         	
         	dialog("暂未开放功能");
             break;
-    case 1:
-    	
-    	
+        case 1:
+	    	mplake=null;
+	    	mplake=MediaPlayer.create(this,R.raw.lake);
+	    	mplake.setOnCompletionListener(mplistener);
     break;
-        case 2:
-            // do something here
+		case 2:
+        	mplake=null;
+        	mplake=MediaPlayer.create(this,R.raw.gq);
+        	mplake.setOnCompletionListener(mplistener);
         	
     break;
         case 3:
@@ -134,25 +126,56 @@ public class MainActivity extends Activity
 
     public  void onrain(View v)
     {
-    	 mprain.start();    	
+    	Button b = (Button)findViewById(R.id.button3);
+    	if(!israinplay)
+    	{
+    		try {
+				mprain.prepare();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	mprain.start();  
+	    	israinplay=true;
+	    	b.setBackgroundResource(R.drawable.main_pause);
+    	}
+    	else
+    	{
+    		mprain.pause();
+	    	israinplay=false;
+	    	b.setBackgroundResource(R.drawable.main_play);
+    	}
     }
     
     public  void onlake(View v)
     {
-    	//mplake.setVolume(10, 10);
-    	try {
-			mplake.prepare();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	mplake.start();        	
+    	Button b = (Button)findViewById(R.id.button4);
+    	if(!islakeplay)
+    	{
+    		try {
+				mplake.prepare();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		mplake.start();  
+	    	islakeplay=true;
+	    	b.setBackgroundResource(R.drawable.main_pause);
+    	}
+    	else
+    	{
+    		mplake.pause();
+    		islakeplay=false;
+	    	b.setBackgroundResource(R.drawable.main_play);
+    	}
     }
-    
-    float lakevol=(float)0.1;
+
     public  void onvol(View v)
     {
     	TextView t = (TextView)findViewById(R.id.textView1);
@@ -164,22 +187,27 @@ public class MainActivity extends Activity
     
     public  void onClick2(View v)
     {
-
-      Button b = (Button)findViewById(R.id.button2);
-      if(b.getTag().toString()!="pause")
-      {
-    	  b.setBackgroundResource(R.drawable.main_pause);
-          b.setTag("pause");
-          mprainlake.start();
-      }
-      else
-      {
-      b.setBackgroundResource(R.drawable.main_play); 
-	  b.setTag("play");
-	  mprainlake.stop();
-      }
-      
     }
+    
+    public void createSDCardDir(){
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+               // 创建一个文件夹对象，赋值为外部存储器的目录
+                File sdcardDir =Environment.getExternalStorageDirectory();
+              //得到一个路径，内容是sdcard的文件夹路径和名字
+                String path=sdcardDir.getPath()+"/rainlake";
+                File path1 = new File(path);
+               if (!path1.exists()) {
+                //若不存在，创建目录，可以在应用启动的时候创建
+                path1.mkdirs();
+                //dialog("paht ok,path:"+path);
+              }
+               }
+        else{
+        	//dialog("paht fail");
+         return;
+
+       }
+       }
     
     /**
      * A placeholder fragment containing a simple view.
@@ -203,19 +231,65 @@ public class MainActivity extends Activity
     @Override
     protected void onStop() {
     	// TODO Auto-generated method stub
-    	if(mprainlake!=null)
-    		mprainlake.release();
-    	if(mprain!=null)
-    		mprain.release();
-    	if(mplake!=null)
-    		mplake.release();
+
+    	
     	super.onStop();
     }
+    
     @Override
     protected void onDestroy() {
     	// TODO Auto-generated method stub
     	super.onDestroy();
-    	
+    	if(mprain!=null)
+    	{
+    		mprain.stop();
+    		mprain.release();
+    	}
+    	if(mplake!=null)
+    	{
+    		mplake.stop();
+    		mplake.release();
+    	}
+    }
+    
+    @Override  
+ // 创建上下文菜单  
+ public void onCreateContextMenu(ContextMenu menu, View v,  
+    ContextMenuInfo menuInfo) {  
+   // 向上下文菜单中添加菜单项  
+   // 注意此处的menu是ContextMenu  
+    	if(( (android.widget.Button)v).getTag().toString().equals("y"))
+    	{
+   menu.add(0, 1, 0, "雨");  
+   menu.add(0, 2, 0, "海");  
+    	}
+    	if(( (android.widget.Button)v).getTag().toString().equals("q"))
+    	{
+   menu.add(0, 3, 0, "钢琴");  
+   menu.add(0, 4, 0, "古琴");  
+    	}
+ } 
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+    	dialog(Integer.toString(item.getItemId()));
+    	switch(item.getItemId()) {
+        case 1:
+            // do something
+    break;
+        case 2:
+            // do something
+    break;
+        case 3:
+            // do something
+    break;
+        case 4:
+            // do something
+    break;
+        default:
+            return super.onContextItemSelected(item);
+        }
+        return true;
+    	//return super.onContextItemSelected(item);
     }
     
     protected void dialog(String  s) {
